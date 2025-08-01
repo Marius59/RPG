1b59a9bffe4822edfadff15f93884a748cebd748
Mjaurius <60984729+Marius59@users.noreply.github.com>
2025-08-01 12:53:14 +0200

    Update Fylkeshusetfantasy.ps1
    lagt til introduksjon og fikset angrep formel

--- Fylkeshusetfantasy.ps1
+++ Fylkeshusetfantasy.ps1
@@ -1,32 +1,37 @@
-$Maincharacter = [PSCustomObject]@{
+   
+    $Maincharacter = [PSCustomObject]@{
     Hp       = 52
     Strenght = 14
     Armour   = 10
+    atkmod  = 1.3
 }
 $slemming =[PSCustomObject]@{
     Name = 'Slemkar'
     Hp = 56
+    Dfcmod = 1.0
 }
 $Bruker = Read-Host "Hvem er du?"
     Write-Host "Velkommen til dette spillet" $bruker
-Function Get-Formula {
-    $currenthp - $damage #* $dmgmodifier * $defencemodifier
-}
 
-
-$state = "True"
+$m= "True"
+While ($m -match "True"){
+   Write-Host "Dette  er en RPG og du er helten/heltinnen i denne historien" "Å nei ser ut til at veien forover er blokkert, du kan angripe ved å skrive angrep(andre måter å slåss på blir lagt til senere)"
+    $state = "True"
     While ($state -match "True"){
         $Action = Read-Host "gjør et angrep"
         Switch($action){
             Angrep{
-            $result = Get-Formula -currenthp $slemming.Hp -damage $Maincharacter.Strenght
-            Write-Host $result
+            $slemming.hp = $slemming.hp - ($Maincharacter.strenght * $Maincharacter.atkmod * $slemming.Dfcmod)
+            Write-host $slemming.hp
             }
             Quit{
-            $state = "false"   
+            $state = "false" 
+            $m = "False"  
             }
-            Test{
-            $slemming.Hp - $Maincharacter.Strenght
+            $slemming.hp -le 0{
+            Write-Host Du har slått fienden
+            $state = "False"
             }
         }
     }   
+}
